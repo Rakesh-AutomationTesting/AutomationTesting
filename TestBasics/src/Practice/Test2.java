61196284d6de977a682b8344a2da6bb0a7bb7ca9
Raks <Raks@Raks-PC>
2020-12-04 12:53:52 +0530

    added a file

---
+++ TestBasics/src/Practice/Test2.java
@@ -0,0 +1,24 @@
+package Practice;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Test2 {
+
+	public static void main(String[] args) throws Exception {
+		
+		System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		
+
+		driver.get("https://www.google.com");
+		driver.navigate().to("https://www.youtube.com");
+		driver.get("https://www.github.com");
+		
+		Thread.sleep(2000);
+		
+		driver.quit();
+		
+	}
+
+}
